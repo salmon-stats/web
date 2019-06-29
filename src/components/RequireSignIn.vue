8b4590983aa136bf8b9b18f6a2e027f83d30fcbf
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-30 01:47:01 +0900

    Create component `RequireSignIn`

---
+++ src/components/RequireSignIn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-if="isSignedIn">
+      <slot></slot>
+    </div>
+    <div v-else>
+      You are required to sign in {{ message }}.
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { metadataModule } from '../store/modules/metadata';
+
+@Component({
+  name: 'RequireSignIn',
+  props: {
+    message: String,
+  }
+})
+export default class RequireSignIn extends Vue {
+  get isSignedIn() {
+    return !!metadataModule.user;
+  }
+}
+</script>
