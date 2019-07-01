9c6ff77ab0a76ea53e3536494d572ff39f59e00b
yukidaruma <yukidaruma@users.noreply.github.com>
2019-07-01 12:31:12 +0900

    Add highlight same weapons on mouseover

--- src/components/MainWeapon.vue
+++ src/components/MainWeapon.vue
@@ -1,18 +1,51 @@
 <template>
-  <img :src="weaponIcon('weapon', weaponId)">
+  <img @mouseover="onMouseover" @mouseleave="onMouseleave"
+    :class="classNames" :src="weaponIcon('weapon', weaponId)">
 </template>
+
+<style scoped>
+.use-highlight.is-highlighted {
+  background-color: rgba(255, 158, 140, 0.25);
+}
+</style>
 
 <script>
 import { Vue, Component } from 'vue-property-decorator';
 import { weaponIcon } from '../helper';
+import { uiStateModule } from '../store/modules/ui-state';
 
 @Component({
   name: 'MainWeapon',
   props: {
     weaponId: [Number, String],
+    highlightOnHover: {
+      type: Boolean,
+      default: true,
+    },
   },
 })
 export default class MainWeapon extends Vue {
   weaponIcon = weaponIcon;
+
+  get isHighlighted() {
+    return uiStateModule.highlightedWeaponId === this.weaponId;
+  }
+  get classNames() {
+    return {
+      'use-highlight': this.highlightOnHover,
+      'is-highlighted': this.isHighlighted,
+    };
+  }
+
+  onMouseover() {
+    if (this.highlightOnHover) {
+      uiStateModule.setHighlightedWeaponId(this.weaponId);
+    }
+  }
+  onMouseleave(e) {
+    if (this.highlightOnHover) {
+      uiStateModule.unsetHighlightedWeaponId();
+    }
+  }
 }
 </script>
