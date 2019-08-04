5bea55399383814ac920685e0d71650f36dfa853
yukidaruma <yukidaruma@users.noreply.github.com>
2019-08-04 23:16:00 +0900

    Add Twitter link separetely

--- src/components/PlayerPageHeader.vue
+++ src/components/PlayerPageHeader.vue
@@ -1,19 +1,23 @@
 <template>
-  <div v-if="user">
-    <h1><a :href="`https://twitter.com/${user.name}`">
-      <player-avatar :size="48" :user="user">
-        @{{ user.name }}
-      </player-avatar>
-    </a></h1>
-  </div>
-  <div v-else>
-    <h1>
-      <player-avatar :size="48" :blockiesSeed="playerId">
-        {{ playerId }}
-      </player-avatar>
-    </h1>
-    <p>{{ playerId }} is not a registered user.</p>
-  </div>
+  <router-link :to="`/players/${playerId}`">
+    <div v-if="user">
+      <h1>
+        <player-avatar :size="48" :user="user">
+          @{{ user.name }}
+        </player-avatar>
+      </h1>
+
+      <a :href="`https://twitter.com/players/${playerId}`">Twitter</a>
+    </div>
+    <div v-else>
+      <h1>
+        <player-avatar :size="48" :blockiesSeed="playerId">
+          {{ playerId }}
+        </player-avatar>
+      </h1>
+      <p>{{ playerId }} is not a registered user.</p>
+    </div>
+  </router-link>
 </template>
 
 <script>
