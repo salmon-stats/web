069c3c28b414ca77fd0d17ee6373629fa2405f3b
yukidaruma <yukidaruma@users.noreply.github.com>
2019-08-10 00:59:53 +0900

    Fix missing import

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,5 @@
+import { IIdKeyMap, idKeyMapModule as idKeyMap } from '@/store/modules/id-key-map';
+
 export const iconUrl = (weaponType: string, id: string | number) => {
   return `https://splatoon-stats-api.yuki.games/static/images/${weaponType}/${id}.png`;
 };
