e950906aa1774c21e1ff9b064bb02dd8373a5b9e
yukidaruma <yukidaruma@users.noreply.github.com>
2019-08-30 03:28:29 +0900

    Add credit in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="home">
-    Home
+    <div>
+      <p>To upload results to this website, please use <a href="https://github.com/tkgstrator/Salmonia/releases">command-line result uploader</a>.</p>
+    </div>
+
+    <p>
+      With contributions from <a href="https://twitter.com/tkgling">@tkgling</a> and <a href="https://twitter.com/barley_ural">@barley_ural</a>,
+      developed &amp; run by <a href="https://twitter.com">@Yukinkling</a>.
+    </p>
+
+    <ul>
+      <li v-for="schedule in schedules">{{ schedule }}</li>
+    </ul>
   </div>
 </template>
