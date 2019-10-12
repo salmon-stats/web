536d266f1c7d71f1f20fe7fc14d9adad238eb1b1
yukidaruma <yukidaruma@users.noreply.github.com>
2019-10-12 22:16:27 +0900

    Use `<player-avatar>` instead of `<img>` in PlayerSearchResult

--- src/components/PlayerSearchResult.vue
+++ src/components/PlayerSearchResult.vue
@@ -2,7 +2,7 @@
   <router-link :to="`/players/${player.player_id}`">
     <div class="box">
       <div class="user">
-        <img v-if="player.twitter_avatar" :src="player.twitter_avatar">
+        <player-avatar v-if="player.twitter_avatar" :user="player" />
         <p class="name"><strong>{{ player.name }}</strong></p>
       </div>
       <p><span class="weak">{{ player.updated_at }}</span></p>
@@ -32,9 +32,11 @@
 
 <script>
 import { Component, Vue, Watch } from 'vue-property-decorator';
+import PlayerAvatar from '@/components/PlayerAvatar.vue';
 
 @Component({
   name: 'PlayerSearchResult',
+  components: { PlayerAvatar },
   props: { player: Object },
 })
 export default class PlayerSearchResult extends Vue {
