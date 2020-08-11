64a23c4ff10d4ebc633a7f19f9e8aefeefd386f1
yukidaruma <yukidaruma@users.noreply.github.com>
2020-08-12 04:38:58 +0900

    fix: Fix dropdown menu is present for single user account
    Rewrites AccountSwitcher

--- src/components/AccountSwitcher.vue
+++ src/components/AccountSwitcher.vue
@@ -1,24 +1,4 @@
-<template>
-  <div>
-    <b-dropdown
-      hoverable
-      position="is-bottom-left"
-    >
-      <router-link :to="`/players/${accounts[0].player_id}`" slot="trigger">
-        <player-avatar style="vertical-align: middle;" :size="32" :user="user" />
-      </router-link>
-      <template v-if="hasMultipleAccounts">
-        <b-dropdown-item v-for="account in accounts" :key="account.player_id" class="dropdown-item">
-          <router-link class="my" :to="`/players/${account.player_id}`">
-            {{ account.name.name }}
-          </router-link>
-        </b-dropdown-item>
-      </template>
-    </b-dropdown>
-  </div>
-</template>
-
-<script lang="ts">
+<script lang="tsx">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import { Account, User } from '../types/salmon-stats';
 
@@ -40,6 +20,33 @@ export default class AccountSwitcher extends Vue {
 
   private get hasMultipleAccounts(): boolean {
     return this.accounts.length > 1;
+  }
+
+  render(h) {
+    const { $props: props } = this;
+
+    const trigger = (
+      <router-link to={`/players/${this.accounts[0].player_id}`}>
+        <player-avatar style="vertical-align: middle;" size={32} show-tooltip={false} user={props.user} />
+      </router-link>
+    );
+
+    return this.hasMultipleAccounts
+      ? (
+        <b-dropdown hoverable position="is-bottom-left">
+          <span slot="trigger">{trigger}</span>
+          {
+            this.accounts.map((account) => (
+              <b-dropdown-item key={account.player_id} class="dropdown-item">
+                <router-link class="my" to={`/players/${account.player_id}`}>
+                  {account.name.name}
+                </router-link>
+              </b-dropdown-item>
+            ))
+          }
+        </b-dropdown>
+      )
+      : trigger;
   }
 }
 </script>
